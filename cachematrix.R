884bbd31fa3dbec1a819123d5501f0fa920f9803
Kanika <kanika.agarwal1904@gmail.com>
2014-07-27 10:36:28 +0530

    computing the inverse of a sqaure matrix
    this solves the problem of inversing the matrix
    
    Signed-off-by: Kanika <kanika.agarwal1904@gmail.com>

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+i<-NULL
+set <- function (matrix){
+  m <<- matrix
+  i << NULL
+}
+get <- function(){
+  m
+}
+getInverse <- function(){
+  i
+}
+list(set = set, get = get,
+     setInverse = setInverse,
+     getInverse = getInverse)
+  
 }
 
 
@@ -12,4 +26,24 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  
+  ## Just return the inverse if its already set
+  if( !is.null(m) ) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  ## Get the matrix from our object
+  data <- x$get()
+  
+  ## Calculate the inverse using matrix multiplication
+  m <- solve(data) %*% data
+  
+  ## Set the inverse to the object
+  x$setInverse(m)
+  
+  ## Return the matrix
+  m
+
 }
